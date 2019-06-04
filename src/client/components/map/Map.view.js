4f48cfc469376bb993228bf7ab0a4eaef42643b5
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-04 10:53:57 -0600

    Removed proj4 dependency

--- src/client/components/map/Map.view.js
+++ src/client/components/map/Map.view.js
@@ -1,5 +1,4 @@
 import mapboxgl from 'mapbox-gl';
-import proj4 from 'proj4';
 import View from '../../superclasses/View';
 
 // eslint-disable-next-line
@@ -9,19 +8,6 @@ export default class MapView extends View {
    */
   constructor(container) {
     super(container);
-
-    proj4.defs([
-      [
-        'EPSG:4326',
-        '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +un' +
-        'its=degrees',
-      ],
-      [
-        'EPSG:3776',
-        '+proj=tmerc +lat_0=0 +lon_0=-114 +k=0.9999 +x_0=0 +y_0=0 +ellps=GRS8' +
-        '0 +datum=NAD83 +units=m +no_defs',
-      ],
-    ]);
 
     mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzbG9yaW5jeiIsImEiOiJjamx5aXVwaH' +
         'AxamZzM3dsaWdkZ3Q2eGJyIn0.mXjlp9c3l2-NBoS1uaEUdw';
