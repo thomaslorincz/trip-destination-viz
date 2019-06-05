8a0c67622f989d40eed903b6f78db1c5b6bdbff5
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-05 14:19:18 -0600

    App is now functional in IE 10 and 11

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,21 @@
-import 'core-js/stable';
+import 'core-js';
+import 'regenerator-runtime/runtime';
+// CustomEvent polyfill
+(function() {
+  if (typeof window.CustomEvent === 'function') return false;
+  // eslint-disable-next-line
+  function CustomEvent(event, params) {
+    params = params || {bubbles: false, cancelable: false, detail: null};
+    const evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(
+        event, params.bubbles, params.cancelable, params.detail
+    );
+    return evt;
+  }
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent;
+})();
+
 import AppModel from './app/App.model';
 import AppPresenter from './app/App.presenter';
 import './style.css';
