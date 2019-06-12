87ddb2c90b34a16144938c5eff6aac43cb5d8623
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-12 14:43:09 -0600

    Production server now redirects HTTP to HTTPS

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -1,10 +1,12 @@
 import path from 'path';
 import express from 'express';
+import sslRedirect from 'heroku-ssl-redirect';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
+app.use(sslRedirect());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
