c268e05e79e1ecb9cdfcd6a89f9e34e2c75db34f
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-12 15:10:04 -0600

    Built prod

---
+++ dist/precache-manifest.05a6131fdd717b9ae705e2864dce4d85.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7934fa93516f6d33407a37e2d1f60930",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "cda9a6eeac064a354b6a",
+    "url": "/main.css"
+  },
+  {
+    "revision": "cda9a6eeac064a354b6a",
+    "url": "/main.js"
+  }
+]);
