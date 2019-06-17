db6a2fef5e16120dddd90489372828b0347a20ad
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-17 12:39:25 -0600

    Remove express-static-gzip code from prod server

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -1,16 +1,14 @@
+import path from 'path';
 import express from 'express';
 import sslRedirect from 'heroku-ssl-redirect';
-import expressStaticGzip from 'express-static-gzip';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(sslRedirect());
 
-app.use('/', expressStaticGzip(__dirname, {
-  enableBrotli: true,
-  customCompressions: [],
-  orderPreference: ['br'],
-}));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
