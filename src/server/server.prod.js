14d8e36bee496e2acbd3b14f29c7d390f0bb4c9b
Thomas Lorincz <tlorincz@ualberta.ca>
2019-07-03 08:30:33 -0600

    Add SSL redirect middleware to prod server config

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -1,12 +1,15 @@
 import path from 'path';
 import express from 'express';
-import sslRedirect from 'heroku-ssl-redirect';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(sslRedirect());
-
+app.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(`https://${req.get('Host')}${req.url}`);
+  }
+  next();
+});
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
