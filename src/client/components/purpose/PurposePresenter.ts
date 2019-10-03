0a43d04960e7e7824c72a96b8b8e384919497778
Thomas Lorincz <tlorincz@ualberta.ca>
2019-10-03 12:35:24 -0600

    Adapt Purpose component to new AppModel logic

--- src/client/components/purpose/PurposePresenter.ts
+++ src/client/components/purpose/PurposePresenter.ts
@@ -7,5 +7,13 @@ export default class PurposePresenter extends Presenter<AppModel, PurposeView> {
   public constructor(model: AppModel, view: PurposeView,
       emitter: EventEmitter) {
     super(model, view, emitter);
+
+    this.emitter.on('purpose-clicked', (purpose: string): void => {
+      this.model.updatePurpose(purpose);
+    });
+
+    this.emitter.on('toggle-purpose-collapse', (): void => {
+      this.model.toggleCollapse('purpose');
+    });
   }
 }
