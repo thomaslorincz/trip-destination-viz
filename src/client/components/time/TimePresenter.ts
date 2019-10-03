e58a895821402f55cd899697c1c25856029db5d4
Thomas Lorincz <tlorincz@ualberta.ca>
2019-10-03 12:42:35 -0600

    Adapt Time component to new AppModel logic

--- src/client/components/time/TimePresenter.ts
+++ src/client/components/time/TimePresenter.ts
@@ -6,5 +6,13 @@ import * as EventEmitter from 'eventemitter3';
 export default class TimePresenter extends Presenter<AppModel, TimeView> {
   public constructor(model: AppModel, view: TimeView, emitter: EventEmitter) {
     super(model, view, emitter);
+
+    this.emitter.on('time-clicked', (time: string): void => {
+      this.model.updateTime(time);
+    });
+
+    this.emitter.on('toggle-time-collapse', (): void => {
+      this.model.toggleCollapse('time');
+    });
   }
 }
--- src/client/components/time/TimeView.ts
+++ src/client/components/time/TimeView.ts
@@ -2,7 +2,49 @@ import View from '../../superclasses/View';
 import * as EventEmitter from 'eventemitter3';
 
 export default class TimeView extends View {
+  private readonly timeEntries: NodeListOf<HTMLElement>;
+  private readonly timeCollapse: HTMLElement;
+
   public constructor(container: HTMLElement, emitter: EventEmitter) {
     super(container, emitter);
+
+    this.timeEntries = document.querySelectorAll('.time-entry');
+    this.timeCollapse = document.getElementById('time-collapse');
+
+    this.timeEntries.forEach((entry: HTMLElement): void => {
+      entry.addEventListener('click', (event: Event): void => {
+        if (!(event.target instanceof HTMLElement)) return;
+
+        this.emitter.emit('time-clicked', event.target.dataset.value);
+      });
+    });
+
+    this.timeCollapse.addEventListener('click', (): void => {
+      this.emitter.emit('toggle-time-collapse');
+    });
+  }
+
+  public draw(times: Map<string, boolean>, collapsed: boolean): void {
+    times.forEach((active: boolean, time: string): void => {
+      const element = document.getElementById(`time-${time}`);
+      const checkbox = element.querySelector('.control-entry-checkbox');
+
+      if (active) {
+        element.classList.add('active');
+        checkbox.textContent = 'check_box';
+      } else {
+        element.classList.remove('active');
+        checkbox.textContent = 'check_box_outline_blank';
+      }
+    });
+
+    const content = document.getElementById('time-content');
+    if (collapsed) {
+      content.classList.add('collapsed');
+      this.timeCollapse.textContent = 'expand_more';
+    } else {
+      content.classList.remove('collapsed');
+      this.timeCollapse.textContent = 'expand_less';
+    }
   }
 }
